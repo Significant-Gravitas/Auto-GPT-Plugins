195d291ad62928f4d83925f38be244da6333d2e1
David Delgado <david@sidewaysthought.net>
2023-05-04 01:48:18 -0500

    Message sends to window without a while loop

--- src/autogpt_plugins/chat_with_user/chat_with_user.py
+++ src/autogpt_plugins/chat_with_user/chat_with_user.py
@@ -1,22 +1,21 @@
-"""Chat With User Class for the plugin."""
-import queue
 import tkinter as tk
 import threading
-import time
+import queue
 
 class ChatWithUserPluginWindow:
+    """This class is used to create the chat window."""
 
     def __init__(
-        self,
-        agent_name,
-        on_message,
+        self, 
+        agent_name, 
+        on_message, 
         on_close
-    ):
-        """Create a chat window.
+    ) -> None:
+        """This method is called when the chat completion is done.
         Args:
             agent_name (str): The name of the agent.
-            on_message (function): The on message function.
-            on_close (function): The on close function.
+            on_message (Callable[[str], None]): The on message callback.
+            on_close (Callable[[], None]): The on close callback.
         """
 
         self.window = tk.Tk()
@@ -32,297 +31,144 @@ class ChatWithUserPluginWindow:
         self.message_queue = queue.Queue()
         self.process_incoming_messages()
 
-    # End of __init__
+    # End of __init__ method
 
 
     def process_incoming_messages(
         self
     ) -> None:
-        """Process the incoming messages."""
+        """This method is called to process the incoming messages."""
 
         while not self.message_queue.empty():
             message = self.message_queue.get()
             self.text_widget.insert(tk.END, self.agent_name + ": " + message + "\n")
         self.window.after(100, self.process_incoming_messages)
 
-    # End of process_incoming_messages
+    # End of process_incoming_messages method
 
 
     def send_message(
         self
     ) -> None:
-        """Send a message."""
+        """This method is called to send the message."""
 
         message = self.entry_widget.get()
         self.entry_widget.delete(0, tk.END)
         self.text_widget.insert(tk.END, "User: " + message + "\n")
         self.on_message(message)
 
-    # End of send_message
+    # End of send_message method
 
 
     def receive_message(
         self, 
         message
     ) -> None:
-        """Receive a message."""
+        """This method is called to receive the message.
+        Args:
+            message (str): The message.
+        """
 
         self.message_queue.put(message)
 
-    # End of receive_message
+    # End of receive_message method
 
-    
+
     def run(
         self
     ) -> None:
-        """Run the window."""
+        """This method is called to run the chat window."""
 
         self.process_incoming_messages()
         self.window.mainloop()
 
-    # End of run
+    # End of run method
+
+# End of ChatWithUserPluginWindow class
 
 
 class ChatWithUserPlugin:
-    """Chat With User Plugin Class."""
+    """This class is used to create the chat window."""
 
     def __init__(
         self, 
         plugin
-    ):
-        """Init the plugin."""
+    ) -> None:
+        """This method is called when the chat completion is done.
+        Args:
+            plugin (AutoGPTPlugin): The plugin.
+        """
 
-        # Save the plugin
         self.plugin = plugin
-
-        # Create a window
         self.window = None
         self.message_event = threading.Event()
+        self.window_created_event = threading.Event()
         self.message = None
 
-    # End of __init__
+    # End of __init__ method
 
 
     def handle_new_message(
-        self,
+        self, 
         message
     ) -> None:
-        """Handle a new message from the user."""
+        """This method is called to handle the new message.
+        Args:
+            message (str): The message.
+        """
 
         self.message = message
         self.message_event.set()
 
-    # End of handle_new_message
+    # End of handle_new_message method
 
 
     def handle_window_close(
         self
-    ):
-        """Handle the window closing."""
+    ) -> None:
+        """This method is called to handle the window close."""
 
         self.window = None
 
-    # End of handle_window_close
+    # End of handle_window_close method
 
 
     def run_chat_window(
         self, 
         agent_name
     ) -> None:
-        """Run the chat window in the main thread."""
+        """This method is called to run the chat window.
+        Args:
+            agent_name (str): The name of the agent.
+        """
 
-        if self.window is None:
-            self.window = ChatWithUserPluginWindow(agent_name, self.handle_new_message, self.handle_window_close)
-            self.window.run()
+        self.window = ChatWithUserPluginWindow(agent_name, self.handle_new_message, self.handle_window_close)
+        self.window_created_event.set()
+        self.window.run()
 
-    # End of run_chat_window
+    # End of run_chat_window method
 
 
     def chat_with_user(
-        self,
-        agent_name = 'AutoGPT',
-        message = '',
-        timeout = 120
+        self, 
+        agent_name='AutoGPT', 
+        message='', 
+        timeout=120
     ) -> str:
-        """Use a desktop window to chat with the user.
+        """This method is called to chat with the user.
         Args:
             agent_name (str): The name of the agent.
             message (str): The message to send.
+            timeout (int): The timeout for the response.
         Returns:
-            str: The resulting response.
+            str: The response from the user.
         """
 
-        """Chat With User Class for the plugin."""
-import tkinter as tk
-import threading
-import queue
-
-class ChatWithUserPluginWindow:
-
-    def __init__(
-        self,
-        agent_name,
-        on_message,
-        on_close
-    ):
-        """Create a chat window.
-        Args:
-            agent_name (str): The name of the agent.
-            on_message (function): The on message function.
-            on_close (function): The on close function.
-        """
-
-        self.window = tk.Tk()
-        self.agent_name = agent_name
-        self.on_message = on_message
-        self.text_widget = tk.Text(self.window)
-        self.entry_widget = tk.Entry(self.window)
-        self.send_button = tk.Button(self.window, text="Send", command=self.send_message)
-        self.text_widget.pack()
-        self.entry_widget.pack()
-        self.send_button.pack()
-        self.window.bind("<Destroy>", lambda e: on_close())
-        self.message_queue = queue.Queue()
-        self.process_incoming_messages()
-
-    # End of __init__
-
-
-    def process_incoming_messages(
-        self
-    ) -> None:
-        """Process the incoming messages."""
-
-        while not self.message_queue.empty():
-            message = self.message_queue.get()
-            self.text_widget.insert(tk.END, self.agent_name + ": " + message + "\n")
-        self.window.after(100, self.process_incoming_messages)
-
-    # End of process_incoming_messages
-
-
-    def send_message(
-        self
-    ) -> None:
-        """Send a message."""
-
-        message = self.entry_widget.get()
-        self.entry_widget.delete(0, tk.END)
-        self.text_widget.insert(tk.END, "User: " + message + "\n")
-        self.on_message(message)
-
-    # End of send_message
-
-
-    def receive_message(
-        self, 
-        message
-    ) -> None:
-        """Receive a message."""
-
-        self.message_queue.put(message)
-
-    # End of receive_message
-
-    
-    def run(
-        self
-    ) -> None:
-        """Run the window."""
-
-        self.process_incoming_messages()
-        self.window.mainloop()
-
-    # End of run
-
-
-class ChatWithUserPlugin:
-    """Chat With User Plugin Class."""
-
-    def __init__(
-        self, 
-        plugin
-    ):
-        """Init the plugin."""
-
-        # Save the plugin
-        self.plugin = plugin
-
-        # Create a window
-        self.window = None
-        self.message_event = threading.Event()
-        self.message = None
-
-    # End of __init__
-
-
-    def handle_new_message(
-        self,
-        message
-    ) -> None:
-        """Handle a new message from the user."""
-
-        self.message = message
-        self.message_event.set()
-
-    # End of handle_new_message
-
-
-    def handle_window_close(
-        self
-    ):
-        """Handle the window closing."""
-
-        self.window = None
-
-    # End of handle_window_close
-
-
-    def run_chat_window(
-        self, 
-        agent_name
-    ) -> None:
-        """Run the chat window in the main thread."""
-        
-        if self.window is None:
-            self.window = ChatWithUserPluginWindow(agent_name, self.handle_new_message, self.handle_window_close)
-            self.window.run()
-
-    # End of run_chat_window
-
-
-    def chat_with_user(
-        self,
-        agent_name = 'AutoGPT',
-        message = '',
-        timeout = 120
-    ) -> str:
-        """Use a desktop window to chat with the user.
-        Args:
-            agent_name (str): The name of the agent.
-            message (str): The message to send.
-        Returns:
-            str: The resulting response.
-        """
-
-        # Run the chat window in the main thread
         threading.Thread(target=self.run_chat_window, args=(agent_name,), daemon=True).start()
 
-        # Wait for the window to be created
-        while self.window is None:
-            time.sleep(0.3)  # Introduce a short sleep interval
-
-        # Display the agent's message in the chat window
+        self.window_created_event.wait()
         self.window.receive_message(message)
-
-        # Wait for the message event and return the message
         self.message_event.wait(timeout)
         self.message_event.clear()
+
         return self.message if self.message else "No response"
-    
-    # End of chat_with_user
-
-
-    
-    # End of chat_with_user
-
