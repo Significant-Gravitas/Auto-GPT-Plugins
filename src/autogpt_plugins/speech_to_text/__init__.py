defd7fface5c0444c74f54a8aedfe5faef783c24
Nigel Phillips <nigelphillips@pursuit.org>
2023-05-06 09:01:03 -0400

    will update later

--- src/autogpt_plugins/speech_to_text/__init__.py
+++ src/autogpt_plugins/speech_to_text/__init__.py
@@ -1,1 +1,20 @@
-.
+from typing import Any, Dict, List, Optional, Tuple, TypedDict, TypeVar
+
+from auto_gpt_plugin_template import AutoGPTPluginTemplate
+
+PromptGenerator = TypeVar("PromptGenerator")
+
+class SpeechToTextPlugin(AutoGPTPluginTemplate):
+    """
+    This is the Auto-GPT Speech-to-Text plugin.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._name = "Auto-GPT-Speech-to-Text-Plugin"
+        self._version = "0.0.1"
+        self._description = "Auto-GPT Speech-to-Text Plugin: Transcribe spoken input in real-time."
+
+    # Add the necessary methods and functionalities as per your requirements
+    # For example, you can add methods like can_handle_post_prompt, post_prompt, etc.
+    # And implement the functionality to transcribe audio and process it through your AutoGPT model
