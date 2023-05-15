dcd4017549f7e8439eb262514e4d834f24a8acb3
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2023-05-15 11:27:06 +0200

    fix test

--- src/autogpt_plugins/scenex/scenex_plugin.py
+++ src/autogpt_plugins/scenex/scenex_plugin.py
@@ -6,6 +6,8 @@ Algorithm = Union["Aqua", "Bolt", "Comet", "Dune", "Ember", "Flash"]
 
 
 class SceneXplain:
+    API_ENDPOINT = "https://us-central1-causal-diffusion.cloudfunctions.net/describe"
+
     def __init__(self, api_key):
         self._api_key = api_key
 
@@ -16,7 +18,6 @@ class SceneXplain:
         features: List[str] = [],
         languages: List[str] = [],
     ) -> str:
-        url = "https://us-central1-causal-diffusion.cloudfunctions.net/describe"
         headers = {
             "x-api-key": f"token {self._api_key}",
             "content-type": "application/json",
@@ -33,7 +34,7 @@ class SceneXplain:
             ]
         }
 
-        response = requests.post(url, headers=headers, json=payload)
+        response = requests.post(self.API_ENDPOINT, headers=headers, json=payload)
         result = response.json().get("result", [])
         img = result[0] if result else {}
 
