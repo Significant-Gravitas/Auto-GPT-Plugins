03c5e208a6dea3f67013f36e20d452b8489dadcd
Samir <118690585+sr5434@users.noreply.github.com>
2023-05-15 11:16:05 -0400

    Add comments to the source code for the Astro plugin

--- src/autogpt_plugins/astro/astronauts.py
+++ src/autogpt_plugins/astro/astronauts.py
@@ -10,6 +10,6 @@ def get_num_astronauts():
     Returns:
         int: The number of astronauts in space.
     """
-    response = requests.get("http://api.open-notify.org/astros.json")
-    data = response.json()
-    return data["number"]
+    response = requests.get("http://api.open-notify.org/astros.json")#Get the data
+    data = response.json()#Convert it to JSON
+    return data["number"]#Extract the number and return it
