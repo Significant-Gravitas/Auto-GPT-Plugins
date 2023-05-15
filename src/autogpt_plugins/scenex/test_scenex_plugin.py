b6737cf5100a876ea2cc5e5125e6fe2754d6ac16
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2023-05-15 11:27:06 +0200

    fix test

--- src/autogpt_plugins/scenex/test_scenex_plugin.py
+++ src/autogpt_plugins/scenex/test_scenex_plugin.py
@@ -1,7 +1,3 @@
-import os
-import unittest
-from unittest.mock import MagicMock, patch
-
 from .scenex_plugin import SceneXplain
 
 MOCK_API_KEY = "secret"
@@ -9,58 +5,41 @@ MOCK_IMAGE = "https://example.com/image.png"
 MOCK_DESCRIPTION = "example description"
 
 
-class TestEmailPlugin(unittest.TestCase):
-    @patch("scenex_plugin.requests.post")
-    def test_describe_image(self, mock_post):
-        mock_post.return_value = MagicMock(
-            json=MagicMock(
-                return_value={
-                    "result": [
-                        {
-                            "image": MOCK_IMAGE,
-                            "text": MOCK_DESCRIPTION,
-                        }
-                    ]
+def test_describe_image(requests_mock):
+    requests_mock.post(
+        SceneXplain.API_ENDPOINT,
+        json={
+            "result": [
+                {
+                    "image": MOCK_IMAGE,
+                    "text": MOCK_DESCRIPTION,
                 }
-            )
-        )
+            ]
+        },
+    )
 
-        scenex = SceneXplain(MOCK_API_KEY)
-        result = scenex.describe_image(
-            image=MOCK_IMAGE,
-            algorithm="Dune",
-            features=[],
-            languages=[],
-        )
+    scenex = SceneXplain(MOCK_API_KEY)
+    result = scenex.describe_image(
+        image=MOCK_IMAGE,
+        algorithm="Dune",
+        features=[],
+        languages=[],
+    )
 
-        # Check the results
-        self.assertEqual(
-            result,
+    # Check the results
+    assert result == {
+        "image": MOCK_IMAGE,
+        "description": MOCK_DESCRIPTION,
+    }
+
+    # Check that the mocked functions were called with the correct arguments
+    requests_mock.request_history[0].json() == {
+        "data": [
             {
                 "image": MOCK_IMAGE,
-                "description": MOCK_DESCRIPTION,
-            },
-        )
-
-        # Check that the mocked functions were called with the correct arguments
-        mock_post.assert_called_once_with(
-            "https://us-central1-causal-diffusion.cloudfunctions.net/describe",
-            headers={
-                "x-api-key": f"token {MOCK_API_KEY}",
-                "content-type": "application/json",
-            },
-            json={
-                "data": [
-                    {
-                        "image": MOCK_IMAGE,
-                        "algorithm": "Dune",
-                        "features": [],
-                        "languages": [],
-                    }
-                ]
-            },
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+                "algorithm": "Dune",
+                "features": [],
+                "languages": [],
+            }
+        ]
+    }
