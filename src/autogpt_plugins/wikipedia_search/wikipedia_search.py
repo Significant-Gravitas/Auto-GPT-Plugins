6b02ddbb233572d08c7fb8e1d29f4a41e88f0a5a
Freddie <epicpupper@outlook.com>
2023-10-09 18:35:53 -0700

    Change user agent to comply with Wikimedia policy
    https://meta.wikimedia.org/wiki/User-Agent_policy

--- src/autogpt_plugins/wikipedia_search/wikipedia_search.py
+++ src/autogpt_plugins/wikipedia_search/wikipedia_search.py
@@ -30,10 +30,7 @@ def _wikipedia_search(query: str, num_results: int = 5) -> str | list[str]:
         session.headers.update(
             {
                 "User-Agent": (
-                    "Mozilla/5.0 (Windows NT 10.0; "
-                    "Win64; x64) AppleWebKit/537.36 "
-                    "(KHTML, like Gecko) Chrome/"
-                    "112.0.5615.49 Safari/537.36"
+                    "AutoGPT wikipedia_search plugin (https://news.agpt.co/contact/) Requests"
                 )
             }
         )
